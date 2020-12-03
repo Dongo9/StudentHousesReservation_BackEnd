9c95418ce7d0fbbf562b2675a57f50ec9d3323d2
manueldonato <manuel.donato@tasgroup.eu>
2020-12-03 10:53:59 +0100

    Global settings (superuser + migrazioni + fix interpreter)

---
+++ StudentHousesReservation_BackEnd/urls.py
@@ -0,0 +1,28 @@
+"""StudentHousesReservation_BackEnd URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from rest_framework.documentation import include_docs_urls
+from rest_framework.schemas import get_schema_view
+
+API_TITLE='Blog API'
+API_DESCRIPTION='A Web API for creating and editing blog posts'
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('docs/', include_docs_urls(title=API_TITLE, description = API_DESCRIPTION)),
+    path('schema/', get_schema_view(title=API_TITLE)),
+]
