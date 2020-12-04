ded02708ae2933188c85e8ba7f6d2dff7f87fb58
manueldonato <manuel.donato@tasgroup.eu>
2020-12-04 14:20:30 +0100

    Changed View implementation with global viewset + urls handled with SimpleRouter

--- allocations/urls.py
+++ allocations/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
 
-from allocations.views import AllocationDetail, AllocationList
+from allocations.views import AllocationViewSet
 
-urlpatterns = [
-    path('<int:pk>/', AllocationDetail.as_view()),
-    path('', AllocationList.as_view()),
-]
+# urlpatterns = [
+#    path('<int:pk>/', AllocationDetail.as_view()),
+#    path('', AllocationList.as_view()),
+# ]
+
+router = SimpleRouter()
+router.register('', AllocationViewSet, basename='allocations')
+
+urlpatterns = router.urls
--- allocations/views.py
+++ allocations/views.py
@@ -1,17 +1,21 @@
 # Create your views here.
-from rest_framework import generics
+from rest_framework import generics, viewsets
 
 from allocations.models import Allocation
 from allocations.serializers import AllocationSerializer
 
 
-class AllocationList(generics.ListCreateAPIView):
-    # FINDALL
-    queryset = Allocation.objects.all()
-    serializer_class = AllocationSerializer
+# class AllocationList(generics.ListCreateAPIView):
+#    # FINDALL
+#    queryset = Allocation.objects.all()
+#    serializer_class = AllocationSerializer
 
 
-class AllocationDetail(generics.RetrieveUpdateDestroyAPIView):
-    # ALLOWED OPERATIONS FOR A SINGLE ALLOCATION ONLY
+# class AllocationDetail(generics.RetrieveUpdateDestroyAPIView):
+# ALLOWED OPERATIONS FOR A SINGLE ALLOCATION ONLY
+#    queryset = Allocation.objects.all()
+#    serializer_class = AllocationSerializer
+
+class AllocationViewSet(viewsets.ModelViewSet):
     queryset = Allocation.objects.all()
     serializer_class = AllocationSerializer
